6a63ffbba85f52a24f53f1079e693890971844dd
Jonathon Hicke <jvhicke@gmail.com>
2026-01-03 16:12:51 -0800

    test update

--- tts/test_tts.py
+++ tts/test_tts.py
@@ -1,5 +1,6 @@
 from tts.tts_service import synthesize
 
 if __name__ == "__main__":
-    output_path = synthesize("Hello, this is a test of the text to speech synthesis service.")
+    test_text = "Hello, this is a test of the text to speech synthesis service."
+    output_path = synthesize(test_text, outputFileName="testfile.wav")
     print (f"testfile generated at: {output_path}")
