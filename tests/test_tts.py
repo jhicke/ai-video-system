57e212f02afc5575a5e656903cfec57a6ff65da4
Jonathon Hicke <jvhicke@gmail.com>
2026-01-05 14:35:53 -0800

    changed to pytest

--- tests/test_tts.py
+++ tests/test_tts.py
@@ -1,6 +1,8 @@
 from tts.tts_service import synthesize
 
-if __name__ == "__main__":
+
+def test_tts_service():
     test_text = "blah Hello, this is a test of the text to speech synthesis service."
     output_path = synthesize(test_text, "testfile.wav")
-    print (f"testfile generated at: {str(output_path)}")
+    print(f"testfile generated at: {str(output_path)}")
+    assert output_path.exists(), "TTS output file was not created."
